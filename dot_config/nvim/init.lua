f276697d37eed2a0b8a1bde3b0d306e9884f6e8a
Adam Cook <adam.j.cook@pm.me>
2023-03-06 08:47:23 -0500

    move to lazyvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,2 @@
--- disable netrw
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrw = 1
-
--- set leader
-vim.g.mapleader = " "
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
-require("plugins")
+-- bootstrap
+require("config.lazy")
