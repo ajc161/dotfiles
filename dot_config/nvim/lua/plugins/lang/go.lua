a6916c01bbe534d15d1e232fab405000a9c04c91
Adam Cook <adam.cook@skiff.com>
2023-04-23 11:35:18 -0400

    feat: add go lsp settings

---
+++ dot_config/nvim/lua/plugins/lang/go.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gosum" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    -- TODO setup format imports on save
+    -- try to use lazyvim hook?
+    -- vim.api.nvim_create_autocmd('BufWritePre', {
+    --   pattern = '*.go',
+    --   callback = function()
+    --     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+    --   end
+    -- })
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            analyses = {
+              nilness = true,
+              shadow = true,
+              unusedparams = true,
+              unusedvariable = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            staticcheck = true,
+          },
+        },
+      },
+    },
+  },
+}
